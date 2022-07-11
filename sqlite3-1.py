dc0231fb0cd4900885f94627b28407a9f3b8c416
Abby Baker <abbybaker269@gmail.com>
2022-07-11 14:47:12 -0700

    sqlite names

--- sqlite3-1.py
+++ sqlite3-1.py
@@ -6,24 +6,26 @@ f_names = ["tony", "clint", "steve", "natasha", "thor", "bruce", "wanda", "pietr
 l_names = ["stark", "barton", "romanoff", "odinson", "banner", "maximoff", "maximoff", "barnes", "rhodes", "parker",
           "fury", "harkness", "lang", "danvers", "wilson", "van dyne", "odinson", "potts", "rambeau", "strange"]
 
+#
+# def create_students(how_many):
+#     for i in range(how_many):
+#         f_name = f_names[random.randint(0, len(f_names) - 1)]
+#         l_name = l_names[random.randint(0, len(l_names) - 1)]
+#         sex = random.choice(['m', 'f'])
+#         print(f"INSERT INTO student(f_name, l_name, sex) VALUES ('{f_name}', '{l_name}', '{sex}');")
+#
+#
+# create_students(10)
+# print()
+#
+#
+# def create_test(num_tests, num_students):
+#     for i in range(1, num_tests + 1):
+#         for j in range(1, num_students + 1):
+#             score = random.randrange(1, 25)
+#             print(f"INSERT INTO test_score VALUES({j}, {i}, {score});")
+#
+#
+# create_test(4, 10)
 
-def create_students(how_many):
-    for i in range(how_many):
-        f_name = f_names[random.randint(0, len(f_names) - 1)]
-        l_name = l_names[random.randint(0, len(l_names) - 1)]
-        sex = random.choice(['m', 'f'])
-        print(f"INSERT INTO student(f_name, l_name, sex) VALUES ('{f_name}', '{l_name}', '{sex}');")
 
-
-create_students(10)
-print()
-
-
-def create_test(num_tests, num_students):
-    for i in range(1, num_tests + 1):
-        for j in range(1, num_students + 1):
-            score = random.randrange(1, 25)
-            print(f"INSERT INTO test_score VALUES({j}, {i}, {score});")
-
-
-create_test(4, 10)
